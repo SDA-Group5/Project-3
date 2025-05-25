854c84981185c8808af83a2a3347df38a8676544
MTeRY01 <mterycraft2011@gmail.com>
2025-05-25 20:51:28 +0300

    rm old files

--- frontend/__mocks__/next-auth/react.ts
+++
@@ -1,5 +0,0 @@
-const useSession = () => ({ data: { user: null }, status: 'unauthenticated' })
-const signIn = () => Promise.resolve({ ok: true })
-const signOut = () => Promise.resolve({ ok: true })
-
-export { useSession, signIn, signOut } 
